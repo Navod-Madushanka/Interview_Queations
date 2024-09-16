d811dc7423231697f76bf19342f6426b7273a20b
Navod Madushanka <navodmaithripala@gmail.com>
2024-09-16 09:54:39 +0530

    Reverse LinkedList

---
+++ src/Leetcode_questions/linked_list/q3/Solution.java
@@ -0,0 +1,29 @@
+package Leetcode_questions.linked_list.q3;
+
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+
+public class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        ListNode head = solution.reverseList(new ListNode(1));
+    }
+}
