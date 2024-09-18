ce210c13efd222b2a6f09ec147aa7d398b786de2
Navod Madushanka <navodmaithripala@gmail.com>
2024-09-18 11:01:09 +0530

    repeat practice

--- src/practice/Solution.java
+++ src/practice/Solution.java
@@ -1,24 +1,35 @@
 package practice;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Solution {
 
-    private int gcd(int a, int b) {
-        if(b == 0) return a;
-        return gcd(b, a % b);
-    }
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies){
+        List<Boolean> result = new ArrayList<Boolean>();
 
-    public String gcdOfStrings(String str1, String str2) {
-        if(!(str1+str2).equals(str2+str1)){
-            return "";
+        int maxCandies = candies[0];
+
+        for(int candy : candies){
+            if(candy > maxCandies){
+                maxCandies = candy;
+            }
         }
 
-        int gcdLength = gcd(str1.length(), str2.length());
-        
-        return str1.substring(0, gcdLength);
+        for(int i = 0; i < candies.length; i++){
+            if(candies[i]+extraCandies >= maxCandies){
+                result.add(true);
+            }else{
+                result.add(false);
+            }
+        }
+
+        return result;
     }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.gcdOfStrings("abcabcabc", "abc"));
+        int[] candies = {2,3,5,1,3};
+        System.out.println(solution.kidsWithCandies(candies, 3));
     }
 }
